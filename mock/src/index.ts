edcaefc595d116177d18fadd0ffc1b8bc7d72b98
detteiu <detteiu.stg@gmail.com>
2020-10-13 10:07:00 +0900

    mock用のapollo serverを動くようにする

--- mock/src/index.ts
+++ mock/src/index.ts
@@ -1,1 +1,34 @@
-console.log("ok2");
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Book {
+    title: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+  },
+  {
+    title: "Jurassic Park",
+  }
+];
+
+const resolvers = {
+  Query: {
+    books: () => books
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Apollo Server ready at ${url}`);
+});
