f1b338026a325de0dc2e7d3a227d74ca6dcb186d
detteiu <detteiu.stg@gmail.com>
2020-10-13 09:18:27 +0900

    mockサーバの下地を準備

---
+++ mock/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+const { NODE_ENV = "production" } = process.env;
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, "src/index.ts")
+  },
+  mode: NODE_ENV,
+  target: "node",
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    alias: {
+      src: path.resolve(__dirname, "src")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ["ts-loader"],
+        exclude: '/node_modules/'
+      }
+    ]
+  }
+};
