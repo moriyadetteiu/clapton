d358270c017f7b81f889080fde4b966d6b935dca
detteiu <detteiu.stg@gmail.com>
2020-10-29 22:09:39 +0900

    #8 docs: 参考リンクを追加

--- mock/src/index.ts
+++ mock/src/index.ts
@@ -1,8 +1,8 @@
 import { ApolloServer, gql } from "apollo-server";
 import { buildClientSchema  } from "graphql";
 
+// https://www.apollographql.com/docs/apollo-server/testing/mocking/#mocking-a-schema-using-introspection
 const introspectionResult = require("./generated/schema.json");
-
 const schema = buildClientSchema(introspectionResult);
 
 const server = new ApolloServer({
