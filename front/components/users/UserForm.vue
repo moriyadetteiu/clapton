773ab4d5f8aaf9dad4675906858b55cf90f46782
detteiu <detteiu.stg@gmail.com>
2020-11-02 19:49:39 +0900

    #22 feat: ユーザ登録フォームの外見を実装

--- front/components/users/UserForm.vue
+++ front/components/users/UserForm.vue
@@ -1,12 +1,39 @@
 <template>
-  <div></div>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-text-field outlined label="名前"/>
+        </v-col>
+        <v-col cols="12">
+          <v-text-field outlined label="ニックネーム"/>
+        </v-col>
+        <v-col cols="12">
+          <v-text-field
+            outlined
+            label="メールアドレス"
+            hint="ログイン時や、パスワードを忘れた際に使用します。"
+            persistent-hint
+          />
+        </v-col>
+        <v-col cols="12">
+          <v-text-field outlined label="パスワード" type="password"/>
+        </v-col>
+        <v-col cols="12">
+          <v-text-field outlined label="パスワード確認" type="password"/>
+        </v-col>
+        <v-col cols="12">
+          <slot />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script lang="ts">
-  import { Vue, Component } from "nuxt-property-decorator";
+  import { Vue, PropSync, Component } from "nuxt-property-decorator";
 
   @Component
   export default class CreateUser extends Vue {
-
   };
 </script>
--- front/pages/users/create.vue
+++ front/pages/users/create.vue
@@ -1,17 +1,20 @@
 <template>
-  <user-form />
+  <user-form>
+    <v-btn color="success">登録</v-btn>
+  </user-form>
 </template>
 
 <script lang="ts">
   import { Vue, Component } from "nuxt-property-decorator";
   import UserForm from "~/components/users/UserForm.vue";
+  // TODO: ~/apollo...を使えるようにする（VSCodeで読み込めるようにする）
+  import { User } from "../../apollo/graphql";
 
-  @Component({
+@Component({
     components: {
       UserForm
     }
   })
   export default class CreateUser extends Vue {
-    name: string = 'hello'
   };
 </script>
