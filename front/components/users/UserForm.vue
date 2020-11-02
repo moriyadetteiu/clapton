4745be7b7b192655f2b73f7ce3516cece9530318
detteiu <detteiu.stg@gmail.com>
2020-11-02 18:51:44 +0900

    #22 feat: ユーザ登録画面のひな型を実装

---
+++ front/components/users/UserForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from "nuxt-property-decorator";
+
+  @Component
+  export default class CreateUser extends Vue {
+
+  };
+</script>
---
+++ front/pages/users/create.vue
@@ -0,0 +1,17 @@
+<template>
+  <user-form />
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from "nuxt-property-decorator";
+  import UserForm from "~/components/users/UserForm.vue";
+
+  @Component({
+    components: {
+      UserForm
+    }
+  })
+  export default class CreateUser extends Vue {
+    name: string = 'hello'
+  };
+</script>
