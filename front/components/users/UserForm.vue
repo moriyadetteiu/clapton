2d0d65678e68351d886cf7327f32987b99833917
detteiu <detteiu.stg@gmail.com>
2020-11-02 23:58:08 +0900

    #22 feat: ユーザ登録のデータ送信するところまで実装

--- front/components/users/UserForm.vue
+++ front/components/users/UserForm.vue
@@ -3,13 +3,18 @@
     <v-container>
       <v-row>
         <v-col cols="12">
-          <v-text-field outlined label="名前"/>
+          <v-text-field
+            v-model="syncedUser.name"
+            label="名前"
+            outlined
+          />
         </v-col>
         <v-col cols="12">
           <v-text-field outlined label="ニックネーム"/>
         </v-col>
         <v-col cols="12">
           <v-text-field
+            v-model="syncedUser.email"
             outlined
             label="メールアドレス"
             hint="ログイン時や、パスワードを忘れた際に使用します。"
@@ -17,7 +22,12 @@
           />
         </v-col>
         <v-col cols="12">
-          <v-text-field outlined label="パスワード" type="password"/>
+          <v-text-field
+            v-model="syncedUser.password"
+            outlined
+            label="パスワード"
+            type="password"
+          />
         </v-col>
         <v-col cols="12">
           <v-text-field outlined label="パスワード確認" type="password"/>
@@ -32,8 +42,13 @@
 
 <script lang="ts">
   import { Vue, PropSync, Component } from "nuxt-property-decorator";
+  import { PropType, PropOptions } from "vue";
+  // TODO: ~/apollo...を使えるようにする（VSCodeで読み込めるようにする）
+  import { UserInput } from "../../apollo/graphql";
 
   @Component
-  export default class CreateUser extends Vue {
+  export default class UserForm extends Vue {
+    @PropSync('user', { type: Object as PropType<UserInput> } as PropOptions<UserInput>)
+    syncedUser!: UserInput;
   };
 </script>
--- front/pages/users/create.vue
+++ front/pages/users/create.vue
@@ -1,20 +1,41 @@
 <template>
-  <user-form>
-    <v-btn color="success">登録</v-btn>
+  <user-form :user="user">
+    <v-btn color="success" @click="submit">登録</v-btn>
   </user-form>
 </template>
 
 <script lang="ts">
+  import 'vue-apollo'
   import { Vue, Component } from "nuxt-property-decorator";
   import UserForm from "~/components/users/UserForm.vue";
   // TODO: ~/apollo...を使えるようにする（VSCodeで読み込めるようにする）
-  import { User } from "../../apollo/graphql";
+  import { UserInput, CreateUserMutation } from "../../apollo/graphql";
 
-@Component({
+  @Component({
     components: {
       UserForm
     }
   })
   export default class CreateUser extends Vue {
+    user: UserInput = {
+      name: "",
+      email: "",
+      password: ""
+    };
+
+    submit(): void {
+      const res = this.$apollo.mutate({
+        mutation: CreateUserMutation,
+        variables: {
+          input: this.user
+        }
+      });
+
+      res.then((result: any) => {
+        console.log(result);
+      }).catch((error: any) => {
+        console.log(error);
+      })
+    }
   };
 </script>
