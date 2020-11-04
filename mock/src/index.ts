1d7ef333592b717f25be7e2dd24d0c2ce0ed0779
detteiu <detteiu.stg@gmail.com>
2020-11-05 00:11:02 +0900

    #22 feat: DateTime型をモックできるようにする

--- mock/src/index.ts
+++ mock/src/index.ts
@@ -1,13 +1,19 @@
-import { ApolloServer, gql } from "apollo-server";
-import { buildClientSchema  } from "graphql";
+import { ApolloServer } from "apollo-server";
+import { buildClientSchema } from "graphql";
+import { IMocks } from 'graphql-tools';
+import scalars from './mocks/scalars';
 
 // https://www.apollographql.com/docs/apollo-server/testing/mocking/#mocking-a-schema-using-introspection
 const introspectionResult = require("./generated/schema.json");
 const schema = buildClientSchema(introspectionResult);
 
+const mocks = {
+  ...scalars
+} as IMocks;
+
 const server = new ApolloServer({
   schema,
-  mocks: true,
+  mocks,
 });
 
 server.listen().then(({ url }) => {
