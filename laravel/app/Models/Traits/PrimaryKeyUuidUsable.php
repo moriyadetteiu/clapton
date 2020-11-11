4f94d5b60ac70cea744097324338e6501bdaaf72
detteiu <detteiu.stg@gmail.com>
2020-11-12 02:53:53 +0900

    #38 docs: 使用上の注意のコメントを追加

--- laravel/app/Models/Traits/PrimaryKeyUuidUsable.php
+++ laravel/app/Models/Traits/PrimaryKeyUuidUsable.php
@@ -4,6 +4,14 @@ namespace App\Models\Traits;
 
 use Illuminate\Support\Str;
 
+/**
+ * モデルを登録する際のid値の生成をuuidに変更する
+ * このTraitを使う際には以下のプロパティをオーバーライドする必要があるので、注意すること
+ * （trait内で、プロパティは定義しないほうがいいので、各クラスで行うこと）
+ *
+ * public $incrementing = false;
+ * protected $keyType = 'string';
+ */
 trait PrimaryKeyUuidUsable
 {
     protected static function bootPrimaryKeyUuidUsable()
