96cd7b3531ec1a88487ed67d8cae3907beef0318
detteiu <detteiu.stg@gmail.com>
2020-11-12 18:52:29 +0900

    #4 feat: ログイン画面をざっくりと実装

---
+++ front/pages/login.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row dense>
+        <v-col cols="12">
+          <v-text-field
+            v-model="credential.email"
+            outlined
+            label="メールアドレス"
+          />
+        </v-col>
+        <v-col cols="12">
+          <v-text-field
+            v-model="credential.password"
+            outlined
+            label="パスワード"
+            type="password"
+          />
+        </v-col>
+        <v-col cols="12">
+          <v-btn color="success" @click="login">ログイン</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import { LoginMutation, Users } from '~/apollo/graphql'
+import UserForm from '~/components/users/UserForm.vue'
+// TODO: ~/apollo...を使えるようにする（VSCodeで読み込めるようにする）
+
+@Component({})
+export default class Login extends Vue {
+  private credential = {
+    email: '',
+    password: ''
+  };
+
+  private async login() {
+    const res = await this.$apollo.mutate({
+      mutation: LoginMutation,
+      variables: this.credential,
+    })
+
+    const token = res.data.login.token;
+
+    if (token) {
+      console.log(token);
+      await this.$apolloHelpers.onLogin(token);
+      this.$router.push('/');
+    }
+  }
+}
+</script>
