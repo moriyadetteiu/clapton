91634fb304d370bc66e22db4b75d97da1ce85542
beryl0123 <ryota.h0616@gmail.com>
2020-11-19 16:10:42 +0900

    #34 feat: イベント作成機能実装

---
+++ laravel/app/Models/Event.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+}
