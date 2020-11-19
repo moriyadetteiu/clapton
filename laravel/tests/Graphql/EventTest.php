84ce41f62e9a0b6233137b8ad8b797d684390d9f
beryl0123 <ryota.h0616@gmail.com>
2020-11-19 16:21:37 +0900

    #34 test:createEventのバックエンドテスト実装

---
+++ laravel/tests/Graphql/EventTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Graphql;
+
+use Illuminate\Foundation\Testing\WithFaker;
+// TODO: テスト用のDBを用意したら有効化する
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+
+class EventTest extends TestCase
+{
+    use WithFaker;
+    // TODO: テスト用のDBを用意したら有効化する
+    // use RefreshDatabase;
+
+    public function testCreateEvent()
+    {
+        $eventInput = [
+            'name' => $this->faker->name,
+        ];
+
+        $response = $this->graphQL('
+            mutation createEvent($input: EventInput!) {
+                createEvent(input: $input) {
+                    id
+                    name
+                }
+            }
+        ', [
+            'input' => $eventInput
+        ]);
+
+        // 登録の確認
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'createEvent' => $eventInput
+                ]
+            ]);
+        $responseData = $response->json()['data']['createEvent'];
+        $this->assertIsUuid($responseData['id']);
+        $this->assertDatabaseHas('events', $responseData);
+    }
+}
