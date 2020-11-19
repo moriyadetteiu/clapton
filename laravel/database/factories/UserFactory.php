d4430a02a507bd238dbab34103e0c4f44df7ebff
detteiu <detteiu.stg@gmail.com>
2020-11-19 18:35:39 +0900

    #4 fix: 追加カラム分を定義

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -24,6 +24,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
+            'name_kana' => $this->faker->name,
+            'handle_name' => $this->faker->name,
+            'handle_name_kana' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
