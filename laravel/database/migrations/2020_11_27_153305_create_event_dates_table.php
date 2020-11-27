0a53ec731710d0b83a4ef5fe39ffaf18f7594a08
beryl0123 <ryota.h0616@gmail.com>
2020-11-28 00:45:10 +0900

    #35 feat: event_datesのマイグレーションファイル作成

---
+++ laravel/database/migrations/2020_11_27_153305_create_event_dates_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventDatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_dates', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->uuid('event_id');
+            $table->string('name');
+            $table->date('date');
+            $table->boolean('is_production_day');
+            $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_dates');
+    }
+}
