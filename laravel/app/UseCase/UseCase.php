2de9936298682abdda0a19a258c46ebb076930da
detteiu <detteiu.stg@gmail.com>
2020-12-01 19:55:45 +0900

    #12 feat: バックエンドにバリデーションを実装

---
+++ laravel/app/UseCase/UseCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\UseCase;
+
+use App\UseCase\UseCaseInput;
+
+abstract class UseCase
+{
+}
---
+++ laravel/app/UseCase/UseCaseInput.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\UseCase;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Support\Facades\Validator as ValidatorFacade;
+
+/**
+ * このクラスの役割は各所からUseCaseを実行する際に、渡す引数の一意性を保つこと
+ * また、渡される引数のバリデーションを行い、後段では妥当性が担保されているようすること
+ */
+abstract class UseCaseInput
+{
+    private array $input = [];
+
+    public function __construct(array $input)
+    {
+        $validator = $this->makeValidator($input);
+
+        $validator->validate();
+
+        $this->input = $input;
+    }
+
+    public function toArray(): array
+    {
+        return $this->input;
+    }
+
+    protected function makeValidator(array $input): Validator
+    {
+        return ValidatorFacade::make($input, $this->rules());
+    }
+
+    abstract protected function rules(): array;
+}
