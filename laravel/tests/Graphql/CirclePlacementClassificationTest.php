87e649d976b1fbf0ecca2e6e2b8b896ad6d8f649
detteiu <detteiu.stg@gmail.com>
2020-12-26 00:43:55 +0900

    #46 feat: 特定のチームに所属する配置分類マスタを返すqueryを追加

--- laravel/tests/Graphql/CirclePlacementClassificationTest.php
+++ laravel/tests/Graphql/CirclePlacementClassificationTest.php
@@ -6,11 +6,12 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Arr;
 
 use App\Models\Team;
+use App\Models\CirclePlacementClassification;
 
 // TODO: テスト用のDBを用意したら有効化する
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class CirclePlacementClassification extends TestCase
+class CirclePlacementClassificationTest extends TestCase
 {
     use WithFaker;
     // TODO: テスト用のDBを用意したら有効化する
@@ -58,5 +59,40 @@ class CirclePlacementClassification extends TestCase
         $this->assertDatabaseHas('circle_placement_classifications', $circlePlacementClassificationInput);
 
         $this->assertEquals($team->id, $responseData['team']['id']);
+    }
+
+    public function testCirclePlacementClassifications()
+    {
+        $team = Team::factory()->create();
+        $team->circlePlacementClassifications->map(fn ($model) => $model->delete());
+
+        $circlePlacementClassifications = CirclePlacementClassification::factory()
+            ->count(3)
+            ->create(['team_id' => $team->id]);
+
+        $response = $this
+            ->actingAsUser()
+            ->graphQL('
+            query circlePlacementClassifications($teamId: ID!) {
+                circlePlacementClassifications(team_id: $teamId) {
+                    id
+                    name
+                    cost
+                    team {
+                        id
+                    }
+                }
+            }
+        ', [
+                'teamId' => $team->id
+            ]);
+
+        $response
+            ->assertStatus(200);
+
+        $responseData = $response->json('data.circlePlacementClassifications');
+        $responseIds = array_column($responseData, 'id');
+        $expectedIds = $circlePlacementClassifications->pluck('id')->all();
+        $this->assertEquals($responseIds, $expectedIds);
     }
 }
