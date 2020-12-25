a93f88a2be050e43fbb5b4e5955cb1ad98b73469
detteiu <detteiu.stg@gmail.com>
2020-12-26 00:58:03 +0900

    #46 feat: 配置分類マスタのページを作成して、紐づくデータを持ってくるところまで実装

---
+++ front/pages/teams/_team_id/circle-placement-classifications.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>aaa</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import {
+  CirclePlacementClassificationsQuery,
+  CirclePlacementClassification,
+} from '~/apollo/graphql'
+
+@Component({
+  apollo: {
+    circlePlacementClassifications: {
+      query: CirclePlacementClassificationsQuery,
+      variables() {
+        const teamId: string = this.$route.params.team_id
+        return { teamId }
+      },
+    },
+  },
+})
+export default class CirclePlacementClassificationPage extends Vue {
+  private circlePlacementClassifications!: CirclePlacementClassification[]
+}
+</script>
