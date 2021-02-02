94a02acbd6cad2853f98a3f3f60d0574501bbf6a
detteiu <detteiu.stg@gmail.com>
2021-02-02 11:36:59 +0900

    #113 refactor: マスタのページを抽象クラスへ切り出す

--- front/pages/teams/_team_id/circle-placement-classifications.vue
+++ front/pages/teams/_team_id/circle-placement-classifications.vue
@@ -1,43 +1,5 @@
-<template>
-  <v-row>
-    <v-col>
-      <circle-placement-classification-form
-        :is-open.sync="isOpenFormDialog"
-        :model="selectedCirclePlacementClassification"
-        :team-id="teamId"
-        @saved="refresh"
-      />
-      <confirm-dialog v-model="isOpenConfirmDialog" @confirmed="remove" />
-      <v-data-table
-        :headers="headers"
-        :items="circlePlacementClassifications"
-        hide-default-footer
-        disable-pagination
-      >
-        <template v-slot:top>
-          <v-toolbar>
-            <v-toolbar-title>配置分類</v-toolbar-title>
-            <v-spacer />
-            <v-btn color="register" @click="create"
-              ><v-icon>mdi-plus</v-icon>追加</v-btn
-            >
-          </v-toolbar>
-        </template>
-        <template v-slot:item.actions="{ item }">
-          <v-btn color="edit" @click="edit(item)"
-            ><v-icon left>mdi-pencil</v-icon>編集</v-btn
-          >
-          <v-btn color="delete" @click="confirmRemove(item)"
-            ><v-icon left>mdi-delete</v-icon>削除</v-btn
-          >
-        </template>
-      </v-data-table>
-    </v-col>
-  </v-row>
-</template>
-
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator'
+import { Component } from 'nuxt-property-decorator'
 import { DataTableHeader } from 'vuetify/types/index'
 import CirclePlacementClassificationForm from '~/components/circle-placement-classification/CirclePlacementClassificationForm.vue'
 import ConfirmDialog from '~/components/dialog/ConfirmDialog.vue'
@@ -46,87 +8,43 @@ import {
   CirclePlacementClassificationsQuery,
   CirclePlacementClassification,
 } from '~/apollo/graphql'
+import AbstractMasterPage from '~/components/masters/AbstractMasterPage.vue'
 
 @Component({
   components: { CirclePlacementClassificationForm, ConfirmDialog },
   apollo: {
-    circlePlacementClassifications: {
+    models: {
       query: CirclePlacementClassificationsQuery,
       variables() {
         const teamId: string = this.$route.params.team_id
         return { teamId }
       },
+      update(data) {
+        return data.circlePlacementClassifications
+      },
     },
   },
 })
-export default class CirclePlacementClassificationPage extends Vue {
-  private circlePlacementClassifications: CirclePlacementClassification[] = []
-  private headers: DataTableHeader[] = [
+export default class CirclePlacementClassificationPage extends AbstractMasterPage<
+  CirclePlacementClassification
+> {
+  protected readonly headers: DataTableHeader[] = [
     { text: '配置分類名', value: 'name' },
     { text: 'コスト', value: 'cost' },
     { text: '操作', value: 'actions', sortable: false },
   ]
 
-  private isOpenFormDialog: boolean = false
-  private isOpenConfirmDialog: boolean = false
-  private selectedCirclePlacementClassification?: CirclePlacementClassification
-
-  private defaultSelectedCirclePlacementClassification(): void {
-    this.selectedCirclePlacementClassification = {
-      id: '',
-      name: '',
-      cost: 0,
-    }
+  protected readonly defaultSelectedModel: CirclePlacementClassification = {
+    id: '',
+    name: '',
+    cost: 0,
   }
 
-  private create(): void {
-    this.defaultSelectedCirclePlacementClassification()
-    this.isOpenFormDialog = true
-  }
+  protected readonly deleteMutation = DeleteCirclePlacementClassificationMutation
 
-  private edit(
-    circlePlacementClassification: CirclePlacementClassification
-  ): void {
-    this.selectedCirclePlacementClassification = circlePlacementClassification
-    this.isOpenFormDialog = true
-  }
+  protected readonly formComponentName: string =
+    'circle-placement-classification-form'
 
-  private confirmRemove(
-    circlePlacementClassification: CirclePlacementClassification
-  ): void {
-    this.selectedCirclePlacementClassification = circlePlacementClassification
-    this.isOpenConfirmDialog = true
-  }
-
-  private remove(): void {
-    const res = this.$apollo.mutate({
-      mutation: DeleteCirclePlacementClassificationMutation,
-      variables: {
-        id: this.selectedCirclePlacementClassification?.id,
-      },
-    })
-
-    res
-      .then(() => {
-        this.$toast.success('削除しました')
-        this.isOpenConfirmDialog = false
-        this.refresh()
-      })
-      .catch(() => {
-        this.$toast.error('削除に失敗しました')
-      })
-  }
-
-  private refresh(): void {
-    this.$apollo.queries.circlePlacementClassifications.refetch()
-  }
-
-  private created(): void {
-    this.defaultSelectedCirclePlacementClassification()
-  }
-
-  private get teamId(): string {
-    return this.$route.params.team_id
-  }
+  protected readonly title: string = '配置分類'
 }
 </script>
