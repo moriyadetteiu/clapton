c7b267171707bce0dd66fb0903e38b5c01865506
detteiu <detteiu.stg@gmail.com>
2021-02-09 20:06:22 +0900

    #24 feat: レイアウトを整える

---
+++ front/pages/mypage/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>todo</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class CreateUser extends Vue {}
+</script>
