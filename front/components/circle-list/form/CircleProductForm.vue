068ab20100d61727c63159cee77bbe171b49edd8
moriyadetteiu <detteiu.stg@gmail.com>
2021-05-08 22:28:21 +0900

    #49 feat: 頒布物のフロントエンドを実装

--- front/components/circle-list/form/CircleProductForm.vue
+++ front/components/circle-list/form/CircleProductForm.vue
@@ -1,10 +1,156 @@
 <template>
-  <div></div>
+  <validation-observer ref="validationObserver" tag="v-form">
+    <v-container>
+      <v-row dense>
+        <v-col cols="12">
+          <v-validate-select
+            v-model="input.circle_product_classification_id"
+            :items="circleProductClassifications"
+            item-text="name"
+            item-value="id"
+            :validation="validation.getItem('circle_product_classification_id')"
+          />
+        </v-col>
+      </v-row>
+
+      <v-row dense>
+        <v-col cols="12">
+          <v-validate-text-field
+            v-model="input.name"
+            :validation="validation.getItem('name')"
+          />
+        </v-col>
+      </v-row>
+
+      <v-row dense>
+        <v-col cols="12">
+          <v-validate-text-field
+            v-model="input.price"
+            type="number"
+            :validation="validation.getItem('price')"
+          />
+        </v-col>
+      </v-row>
+
+      <v-row dense>
+        <v-col cols="12">
+          <v-btn color="success" @click="submit">登録</v-btn>
+          <v-btn @click="$emit('canceled')">キャンセル</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </validation-observer>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator'
+import { Vue, Prop, Component, Watch } from 'nuxt-property-decorator'
+import { PropType } from 'vue'
+import { ValidationObserver } from 'vee-validate'
+import { isApolloError } from 'apollo-client/errors/ApolloError'
+import { MutationOptions } from 'apollo-client'
+import {
+  CircleProduct,
+  CircleProductInput,
+  CircleProductClassification,
+  CircleProductClassificationsQuery,
+  CreateCircleProductMutation,
+  UpdateCircleProductMutation,
+} from '~/apollo/graphql'
+import { CreateCircleProductInputValidation } from '~/validation/validations'
 
-@Component
-export default class CircleProductForm extends Vue {}
+const initialCircleProductInput: CircleProductInput = {
+  circle_product_classification_id: '',
+  name: '',
+  price: 0,
+}
+
+@Component({
+  apollo: {
+    circleProductClassifications: {
+      query: CircleProductClassificationsQuery,
+      variables() {
+        const teamId = this.teamId
+        return { teamId }
+      },
+    },
+  },
+})
+export default class CircleProductForm extends Vue {
+  private validation: CreateCircleProductInputValidation = new CreateCircleProductInputValidation()
+
+  private input: CircleProductInput = { ...initialCircleProductInput }
+
+  private circleProductClassifications: CircleProductClassification[] = []
+
+  @Prop({ type: String, required: true })
+  private teamId!: string
+
+  @Prop({ type: String, required: true })
+  private circlePlacementId!: string
+
+  @Prop({ type: Object as PropType<CircleProduct> })
+  private circleProduct?: CircleProduct
+
+  $refs!: {
+    validationObserver: InstanceType<typeof ValidationObserver>
+  }
+
+  @Watch('circleProduct', { immediate: true })
+  private onUpdateCircleProduct(): void {
+    if (!this.circleProduct) {
+      this.input = { ...initialCircleProductInput }
+      return
+    }
+    const input = {}
+    Object.keys(initialCircleProductInput).forEach((key) => {
+      ;(input as any)[key] = (this.circleProduct as any)[key]
+    })
+    this.input = input as CircleProductInput
+  }
+
+  private async submit() {
+    const observer = this.$refs.validationObserver
+    const isValid = await observer.validate()
+    if (isValid) {
+      const mutationName = this.isCreate
+        ? 'createCircleProductMutation'
+        : 'updateCircleProductMutation'
+      const circleProduct = await this.$apollo
+        .mutate(this.mutateOption)
+        .then((res) => res.data![mutationName])
+        .catch((error) => {
+          if (isApolloError(error)) {
+            this.$toasted.global.validationError()
+            this.validation.setBackendErrorsFromAppolo(error)
+          }
+        })
+      this.$toast.success('保存しました')
+      this.$emit('saved', { circleProduct })
+    }
+  }
+
+  private get isCreate(): boolean {
+    return !this.circleProduct?.id
+  }
+
+  private get mutateOption(): MutationOptions {
+    const input = {
+      ...this.input,
+      circle_placement_id: this.circlePlacementId,
+    }
+    if (this.isCreate) {
+      return {
+        mutation: CreateCircleProductMutation,
+        variables: { input },
+      }
+    }
+    return {
+      mutation: UpdateCircleProductMutation,
+      variables: {
+        id: this.circleProduct!.id,
+        input,
+      },
+    }
+  }
+}
 </script>
