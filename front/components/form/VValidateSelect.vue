526dd9c966453e73f225e395cae595f8cca9dd85
moriyadetteiu <detteiu.stg@gmail.com>
2021-05-08 19:27:35 +0900

    #148 fix: componentであることを修飾する

--- front/components/form/VValidateSelect.vue
+++ front/components/form/VValidateSelect.vue
@@ -15,8 +15,13 @@
 </template>
 
 <script lang="ts">
+import { Component } from 'nuxt-property-decorator'
 import AbstractVValidateInput from './AbstractVValidateInput.vue'
 
+// @ts-ignore
+@Component({
+  inheritAttrs: false,
+})
 export default class VValidateSelect extends AbstractVValidateInput {
   protected readonly inputComponentName: string = 'v-select'
 }
