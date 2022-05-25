6f7a887efe5c2bae96ad716897f6adbe09fe5ba9
moriyadetteiu <detteiu.stg@gmail.com>
2022-05-26 02:32:53 +0900

    #181 fix: 頒布物が自分が欲しいものではない場合の表示制御を追加

---
+++ front/components/circle-list/form/CircleProductRow.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-row>
+    <v-col cols="12" class="text--primary pb-0">
+      {{ circleProduct.circleProductClassification.name }}
+      {{ circleProduct.name }}
+      <template v-if="hasMyWantCircleProduct">
+        <v-btn @click="editCircleProduct">編集</v-btn>
+        <v-btn @click="deleteCircleProduct">削除</v-btn>
+      </template>
+    </v-col>
+    <v-col cols="12" class="pt-0">
+      購入者:
+      <v-chip
+        v-for="wantCircleProduct in circleProduct.wantCircleProducts"
+        :key="wantCircleProduct.id"
+        small
+        class="mr-2"
+        :color="
+          wantCircleProduct.careAboutCircle.joinEvent.user.id === me.id
+            ? `primary`
+            : ``
+        "
+      >
+        {{ wantCircleProduct.careAboutCircle.joinEvent.user.name }}
+      </v-chip>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+import { Vue, Prop, Component, Emit } from 'nuxt-property-decorator'
+import {
+  CircleProduct,
+  DeleteCircleProductMutation,
+  User,
+  WantCircleProduct,
+} from '~/apollo/graphql'
+import { userStore } from '~/store'
+
+@Component({})
+export default class CircleListRow extends Vue {
+  @Prop({ type: Object as PropType<CircleProduct>, required: true })
+  private circleProduct!: CircleProduct
+
+  @Emit('edit-circle-product')
+  private editCircleProduct(): CircleProduct {
+    return this.circleProduct
+  }
+
+  @Emit('delete-circle-product')
+  private onDeleteCircleProduct(): CircleProduct {
+    return this.circleProduct
+  }
+
+  private get me(): User {
+    return userStore.loginUserOrEmptyUser
+  }
+
+  private get myWantCircleProduct(): WantCircleProduct | null {
+    return (
+      this.circleProduct.wantCircleProducts.find(
+        (wantCircleProduct) =>
+          wantCircleProduct?.careAboutCircle?.joinEvent?.user?.id === this.me.id
+      ) || null
+    )
+  }
+
+  private get hasMyWantCircleProduct(): boolean {
+    return this.myWantCircleProduct !== null
+  }
+
+  private async deleteCircleProduct() {
+    const id = this.circleProduct.id
+    await this.$apollo
+      .mutate({
+        mutation: DeleteCircleProductMutation,
+        variables: { id },
+      })
+      .catch(() => this.$toast.error('削除に失敗しました'))
+
+    this.onDeleteCircleProduct()
+    this.$toast.success('削除しました')
+  }
+
+  private wantMeToo() {}
+}
+</script>
