8e40d58e8149feb661d8924655402817548b6185
moriyadetteiu <detteiu.stg@gmail.com>
2022-06-03 19:20:57 +0900

    #223 feat: チーム作成時には作成者は自動で所属されるようにする

--- laravel/tests/Graphql/TeamTest.php
+++ laravel/tests/Graphql/TeamTest.php
@@ -40,5 +40,9 @@ class TeamTest extends TestCase
         $this->assertIsUuid($responseData['id']);
         $this->assertTrue(preg_match('/^[0-9a-f]{13}$/', $responseData['code']) === 1);
         $this->assertDatabaseHas('teams', $responseData);
+        $this->assertDatabaseHas('user_affiliation_teams', [
+            'user_id' => $this->loginUser->id,
+            'team_id' => $responseData['id'],
+        ]);
     }
 }
