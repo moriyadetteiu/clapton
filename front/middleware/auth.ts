d3811a0bcbdf7e10383298bbc172c1bdd9cfbfef
moriyadetteiu <detteiu.stg@gmail.com>
2022-06-09 18:46:22 +0900

    #240 fix: 認証が切れているときに一瞬エラー画面が表示される不具合を修正

--- front/middleware/auth.ts
+++ front/middleware/auth.ts
@@ -14,6 +14,7 @@ export default async ({
   route,
   $config,
   $axios,
+  redirect,
 }: Context & { $axios: any }) => {
   // csrf tokenの更新を行う
   // note: csrf tokenはcookieに書き込まれる（ブラウザで処理される）ため、レスポンスの処理はせずにリクエストするだけでOK
@@ -22,10 +23,14 @@ export default async ({
   const isUseMock = $config.IS_USE_MOCK_SERVER
   const routeName = route.name ?? ''
   if (!isUseMock && !canAccessGuest(routeName)) {
-    // note: ログインできていない場合、ここからapolloErrorLinkのUnauthenticatedのログアウト / リダイレクト処理が走る
-    const user = await app.$defaultApolloClient
-      .query({ query: MeQuery, fetchPolicy: 'no-cache' })
-      .then((result: any) => result.data.me)
-    userStore.setLoginUser(user)
+    try {
+      const user = await app.$defaultApolloClient
+        .query({ query: MeQuery, fetchPolicy: 'no-cache' })
+        .then((result: any) => result.data.me)
+      userStore.setLoginUser(user)
+    } catch (e) {
+      // note: 明示的にredirect関数を呼び出し、返さないと一瞬遷移先のページが表示されてしまうので、これは必要
+      return redirect('/login')
+    }
   }
 }
