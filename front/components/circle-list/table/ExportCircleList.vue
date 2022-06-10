7ec594e7dd6170aaebac1f60d1ea26ddb2cc15f4
moriyadetteiu <detteiu.stg@gmail.com>
2022-06-10 18:42:00 +0900

    #54 feat: excelダウンロードのフロントエンド実装

---
+++ front/components/circle-list/table/ExportCircleList.vue
@@ -0,0 +1,104 @@
+<template>
+  <v-dialog v-model="isOpenSync">
+    <v-card>
+      <v-card-title>Excelダウンロード</v-card-title>
+      <v-card-text>
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-header>詳細設定</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-row>
+                <v-col cols="4">出力項目</v-col>
+                <v-col cols="8">
+                  <v-chip-group
+                    v-model="selectedExportColumnIndexes"
+                    active-class="primary"
+                    multiple
+                    column
+                  >
+                    <v-chip
+                      v-for="exportColumnCandidate in exportColumnCandidates"
+                      :key="exportColumnCandidate.name"
+                    >
+                      {{ exportColumnCandidate.label }}
+                    </v-chip>
+                  </v-chip-group>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="4">シートを分ける基準</v-col>
+                <v-col cols="8">
+                  <v-chip-group
+                    v-model="selectedGroupingColumnIndexes"
+                    active-class="primary"
+                    multiple
+                    column
+                  >
+                    <v-chip
+                      v-for="groupingColumnCandidate in groupingColumnCandidates"
+                      :key="groupingColumnCandidate.name"
+                    >
+                      {{ groupingColumnCandidate.label }}
+                    </v-chip>
+                  </v-chip-group>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" @click="download">ダウンロード</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+import { Vue, Prop, PropSync, Component, Watch } from 'nuxt-property-decorator'
+import TableStateInterface, {
+  ExportColumnCandidate,
+} from './TableStateInterface'
+
+@Component({})
+export default class ExportCircleList extends Vue {
+  @PropSync('isOpen', { type: Boolean, required: true })
+  private isOpenSync!: Boolean
+
+  @Prop({ type: Object as PropType<TableStateInterface>, required: true })
+  private readonly tableState!: TableStateInterface
+
+  @Prop({ type: Array as PropType<string[]>, required: true })
+  private readonly circleListIds!: string[]
+
+  private selectedExportColumnIndexes: number[] = []
+
+  private selectedGroupingColumnIndexes: number[] = []
+
+  private get exportColumnCandidates(): ExportColumnCandidate[] {
+    return this.tableState.getExportColumnCandidates()
+  }
+
+  private get selectedExportColumnNames(): string[] {
+    return this.selectedExportColumnIndexes.map(
+      (index) => this.exportColumnCandidates[index].columnName
+    )
+  }
+
+  private get groupingColumnCandidates(): ExportColumnCandidate[] {
+    return this.tableState.getExportGroupingColumnCandidates()
+  }
+
+  @Watch('exportColumnCandidates', { immediate: true })
+  private onUpdateExportColumnCandidates(): void {
+    this.selectedExportColumnIndexes = this.exportColumnCandidates.map(
+      (_, index) => index
+    )
+  }
+
+  private download(): void {
+    // TODO: ダウンロード処理
+  }
+}
+</script>
--- front/components/circle-list/table/MyCircleListTableState.ts
+++ front/components/circle-list/table/MyCircleListTableState.ts
@@ -1,22 +1,18 @@
-import { DataTableHeader } from 'vuetify/types/index'
-import TableStateInterface from './TableStateInterface'
-import TableHeaderDefinition from './TableHeaderDefinitions'
+import AbstractTableState from './AbstractTableState'
 
-const tableHeaderKeys = [
-  'event_date_name',
-  'placement_full',
-  'circle_name',
-  'circle_placement_classification_name',
-  'circle_product_classification_name',
-  'circle_product_name',
-  'circle_product_price',
-  'want_circle_product_quantity',
-  'want_priority_name',
-  'circle_memo',
-]
+export default class MyCircleListTableState extends AbstractTableState {
+  protected tableHeaderKeys: string[] = [
+    'event_date_name',
+    'placement_full',
+    'circle_name',
+    'circle_placement_classification_name',
+    'circle_product_classification_name',
+    'circle_product_name',
+    'circle_product_price',
+    'want_circle_product_quantity',
+    'want_priority_name',
+    'circle_memo',
+  ]
 
-export default class MyCircleListTableState implements TableStateInterface {
-  getTableHeaders(): DataTableHeader[] {
-    return TableHeaderDefinition.getTableHeaders(tableHeaderKeys)
-  }
+  protected exportGroupingColumnKeys: string[] = ['event_date_name']
 }
