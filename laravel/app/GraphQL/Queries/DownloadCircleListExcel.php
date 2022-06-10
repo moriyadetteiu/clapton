aee5b231aabbc3c72132caf10ee3f1af8324b5e6
moriyadetteiu <detteiu.stg@gmail.com>
2022-06-10 19:37:44 +0900

    #54 chore: laravel excelをインストール

---
+++ laravel/app/GraphQL/Queries/DownloadCircleListExcel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+class DownloadCircleListExcel
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+    }
+}
