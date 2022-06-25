939db36ac983ca57e065e9db519b7469c9df778e
moriyadetteiu <detteiu.stg@gmail.com>
2022-06-25 20:36:34 +0900

    #53 feat: お気に入り一覧を見れるようにする

---
+++ front/components/favorites/FavoriteTable.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="favorites"
+    hide-default-footer
+    disable-pagination
+    fixed-header
+  >
+    <template #top>
+      <v-card>
+        <v-card-text>
+          <v-tooltip top>
+            <template #activator="{ on, attrs }">
+              <v-row v-on="on" v-bind="attrs">
+                <v-switch
+                  v-model="isEditable"
+                  label="編集"
+                  dense
+                  hide-details
+                  class="mt-0 pl-3"
+                />
+              </v-row>
+            </template>
+            <span>
+              有効にすると、お気に入りの解除が行えるようになります。
+            </span>
+          </v-tooltip>
+        </v-card-text>
+      </v-card>
+    </template>
+    <template #[`item.circle.name`]="{ item }">
+      <favorite-button v-if="isEditable" :circle-id="item.circle.id" />
+      {{ item.circle.name }}
+    </template>
+  </v-data-table>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import { DataTableHeader } from 'vuetify/types/index'
+import { Favorite } from '~/apollo/graphql'
+import FavoriteButton from './FavoriteButton.vue'
+
+@Component({
+  components: {
+    FavoriteButton,
+  },
+})
+export default class FavoriteTable extends Vue {
+  @Prop({
+    type: Array as PropType<Favorite[]>,
+    required: true,
+  })
+  private favorites!: Favorite[]
+
+  private readonly headers: DataTableHeader[] = [
+    {
+      text: 'サークル名',
+      value: 'circle.name',
+    },
+  ]
+
+  private isEditable: boolean = false
+}
+</script>
