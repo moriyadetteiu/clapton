c02e4156ad5589e3395e732fd4452e608d1e0589
moriyadetteiu <detteiu.stg@gmail.com>
2022-07-07 20:16:33 +0900

    #301 fix: 同一のCareAboutCircleが作られないようにする

--- laravel/app/UseCase/CareAboutCircle/CreateCareAboutCircle.php
+++ laravel/app/UseCase/CareAboutCircle/CreateCareAboutCircle.php
@@ -9,7 +9,20 @@ class CreateCareAboutCircle extends UseCase
 {
     public function execute(CreateCareAboutCircleInput $input)
     {
-        $careAboutCircle = CareAboutCircle::create($input->toArray());
+        $careAboutCircle = $this->findOrCreateCareAboutCircle($input);
         return $careAboutCircle;
+    }
+
+    private function findOrCreateCareAboutCircle(CreateCareAboutCircleInput $input)
+    {
+        $careAboutCircle = CareAboutCircle::where('join_event_id', $input->get('join_event_id'))
+            ->where('circle_placement_id', $input->get('circle_placement_id'))
+            ->first();
+
+        if ($careAboutCircle) {
+            return $careAboutCircle;
+        }
+
+        return CareAboutCircle::create($input->toArray());
     }
 }
