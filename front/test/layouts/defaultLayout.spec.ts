d2807c133e97db3c8c63894e6f3c873f1a48f6ac
moriyadetteiu <detteiu.stg@gmail.com>
2022-07-26 02:47:28 +0900

    #330 test: 表示されなくなった文言のテストケースを修正

--- front/test/layouts/defaultLayout.spec.ts
+++ front/test/layouts/defaultLayout.spec.ts
@@ -33,7 +33,7 @@ describe('default layout', () => {
     const wrapper = makeWrapper()
 
     await flushPromises()
-    expect(wrapper.text()).toContain('clapton')
+    expect(wrapper.text()).toContain('ログイン')
     expect(wrapper.text()).not.toContain('ログアウト')
   })
 
@@ -59,7 +59,6 @@ describe('default layout', () => {
     })
 
     await flushPromises()
-    expect(wrapper.text()).toContain('clapton')
     expect(wrapper.text()).toContain('ログアウト')
     expect(wrapper.text()).toContain(user.name)
     expect(wrapper.text()).toContain(event.name)
