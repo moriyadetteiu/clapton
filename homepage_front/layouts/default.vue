47e910afe54f785cff99af037489b252c8619b04
moriyadetteiu <detteiu.stg@gmail.com>
2022-07-26 18:18:45 +0900

    #335 chore: ホームページ用のひな型を作成

---
+++ homepage_front/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app> header </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer app>
+      <span> footer </span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class DefaultLayout extends Vue {}
+</script>
---
+++ homepage_front/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>home</v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class IndexPage extends Vue {}
+</script>
